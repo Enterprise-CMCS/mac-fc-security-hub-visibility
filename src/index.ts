29bd9aefffd4a7b4810c1ec3d6ef37c991306e2c
andrew-corbalt <130405742+andrew-corbalt@users.noreply.github.com>
2024-03-14 20:28:35 -0700

    [CMCSMACD-2070] Refactor to merge macpro code into macfc action (#14)
    * Update this code to no longer depend on the external MACPro repo.

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,71 @@
-import * as core from '@actions/core'
-import {SecurityHubJiraSync} from '@enterprise-cmcs/macpro-security-hub-sync';
+import * as core from '@actions/core';
+import { SecurityHubJiraSync } from './macfc-security-hub-sync';
 
-async function run(): Promise<void> {
-  try {
-    process.env.JIRA_HOST = core.getInput('jira-host')
-    process.env.JIRA_USERNAME = core.getInput('jira-username')
-    process.env.JIRA_TOKEN = core.getInput('jira-token')
-    process.env.JIRA_PROJECT = core.getInput('jira-project-key')
-    process.env.JIRA_CLOSED_STATUSES = core.getInput('jira-ignore-statuses')
-    process.env.AUTO_CLOSE = core.getInput('auto-close')
-    process.env.ASSIGNEE = core.getInput('assign-jira-ticket-to')
-
-    let customJiraFields
-    const customJiraFieldsInput = core.getInput('jira-custom-fields')
-    if (customJiraFieldsInput) {
-      try {
-        customJiraFields = JSON.parse(customJiraFieldsInput)
-      } catch (e: any) {
-        throw new Error(
-          `Error parsing JSON string for jira-custom-fields input: ${e.message}`
-        )
-      }
-    }
-
-    core.info('Syncing Security Hub and Jira')
-    await new SecurityHubJiraSync({
-      region: core.getInput('aws-region'),
-      severities: core
-        .getInput('aws-severities')
-        .split(',')
-        .map(severity => severity.trim()),
-      epicKey: core.getInput('jira-epic-key'),
-      customJiraFields
-    }).sync()
-  } catch (e: any) {
-    core.setFailed(`Sync failed: ${e.message}`)
+// Utility function to get input with fallback to environment variable
+function getInputOrEnv(inputName: string, envName: string) {
+  const inputValue = core.getInput(inputName);
+  if (inputValue !== '') {
+    process.env[envName] = inputValue;
+    return
   }
 }
 
-run()
+function validateAndFilterSeverities(inputSeverities: string | undefined): string[] | undefined{
+  if (!inputSeverities) {
+    return undefined; // Potentially update with default severities
+  }
+  const allowedSeverities = ["INFORMATIONAL", "LOW", "MEDIUM", "HIGH", "CRITICAL"];
+
+  const inputSeveritiesArray = inputSeverities.split(',')
+    .map(severity => severity.trim().toUpperCase())
+    .filter(severity => severity);
+
+  // Check each severity in the array against the allowed severities
+  inputSeveritiesArray.forEach(severity => {
+    if (!allowedSeverities.includes(severity)) {
+      throw new Error(`Invalid severity level detected: '${severity}'. Allowed severities are: ${allowedSeverities.join(', ')}.`);
+    }
+  });
+
+  return inputSeveritiesArray;
+}
+
+async function run(): Promise<void> {
+  try {
+    getInputOrEnv('jira-base-uri', 'JIRA_BASE_URI');
+    getInputOrEnv('jira-host', 'JIRA_HOST');
+    getInputOrEnv('jira-username', 'JIRA_USERNAME');
+    getInputOrEnv('jira-token', 'JIRA_TOKEN');
+    getInputOrEnv('jira-project-key', 'JIRA_PROJECT');
+    getInputOrEnv('jira-ignore-statuses', 'JIRA_CLOSED_STATUSES');
+    getInputOrEnv('auto-close', 'AUTO_CLOSE');
+    getInputOrEnv('assign-jira-ticket-to', 'ASSIGNEE');
+    getInputOrEnv('aws-region', 'AWS_REGION');
+    getInputOrEnv('aws-severities', 'AWS_SEVERITIES')
+    getInputOrEnv('dry-run', 'DRY_RUN');
+
+    let customJiraFields;
+    getInputOrEnv('jira-custom-fields', 'JIRA_CUSTOM_FIELDS');
+    if (process.env.JIRA_CUSTOM_FIELDS) {
+      try {
+        customJiraFields = JSON.parse(process.env.JIRA_CUSTOM_FIELDS);
+      } catch (e: unknown) {
+        throw new Error(
+          `Error parsing JSON string for jira-custom-fields input: ${e}`
+        );
+      }
+    }
+
+    core.info('Syncing Security Hub and Jira');
+    await new SecurityHubJiraSync({
+      region: process.env.AWS_REGION,
+      severities:  validateAndFilterSeverities(process.env.AWS_SEVERITIES),
+      epicKey: process.env.JIRA_EPIC_KEY,
+      customJiraFields
+    }).sync();
+  } catch (e: any) {
+    core.setFailed(`Sync failed: ${e.message}`);
+  }
+}
+
+run();
