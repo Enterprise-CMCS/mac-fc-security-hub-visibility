d75c703a8dc309a55dee441532b46512e87e3aeb
maira-samtek <maira@samtek.io>
2024-08-01 05:35:12 -0400

    PR check should fail

--- src/macfc-security-hub-sync.ts
+++ src/macfc-security-hub-sync.ts
@@ -397,7 +397,6 @@ export class SecurityHubJiraSync {
       }
     }
     let newIssueInfo
-    console.log("I am test")
     try {
       newIssueInfo = await this.jira.createNewIssue(newIssueData)
       const issue_id = this.jiraLinkId
